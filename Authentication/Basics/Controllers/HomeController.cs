023d8ac37678f277477d5aeb63bb38ca75c6cc15
Jaunius Pinelis <jaunius@aml-analytics.com>
2020-08-21 15:09:38 +0300

    Completed basics

--- Authentication/Basics/Controllers/HomeController.cs
+++ Authentication/Basics/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -23,7 +25,26 @@ namespace Basics.Controllers
 
         public IActionResult Authenticate()
 		{
-            return RedirectToAction("Action");
+            var grandmaClaims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name, "Bob"),
+                new Claim(ClaimTypes.Email, "Bob@gmail.com"),
+                new Claim("Grandma.Says", "Very nice boi.")
+            };
+
+            var licenseClaims = new List<Claim>() {
+              new Claim(ClaimTypes.Name, "Bob K Foo"),
+              new Claim("DrivingLicense", "A+")
+            };
+
+            var grandmaIdentity = new ClaimsIdentity(grandmaClaims, "Grandma Identity");
+            var licenseIdentity = new ClaimsIdentity(licenseClaims, "Government");
+
+            var userPrincipal = new ClaimsPrincipal(new[] { grandmaIdentity, licenseIdentity });
+
+            HttpContext.SignInAsync(userPrincipal);
+
+            return RedirectToAction("Index");
 		}
 
     }
--- Authentication/Basics/Startup.cs
+++ Authentication/Basics/Startup.cs
@@ -16,6 +16,13 @@ namespace Basics
 		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
 		public void ConfigureServices(IServiceCollection services)
 		{
+			services.AddAuthentication("CookieAuth")
+				.AddCookie("CookieAuth", config =>
+				{
+					config.Cookie.Name = "Grandmas.Cookie";
+					config.LoginPath = "/Home/Authenticate";
+				});
+
 			services.AddControllersWithViews();
 		}
 
@@ -28,6 +35,10 @@ namespace Basics
 			}
 
 			app.UseRouting();
+
+			app.UseAuthentication();
+
+			app.UseAuthorization();
 
 			app.UseEndpoints(endpoints =>
 			{
