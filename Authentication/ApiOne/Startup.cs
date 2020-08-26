f42868ebb7b8dcc43ab6215b97d6c3f0db48be4d
Jaunius Pinelis <jaunius@aml-analytics.com>
2020-08-26 17:53:57 +0300

    Completing api2

--- Authentication/ApiOne/Startup.cs
+++ Authentication/ApiOne/Startup.cs
@@ -12,8 +12,6 @@ namespace ApiOne
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddAuthentication("Bearer")
@@ -27,7 +25,6 @@ namespace ApiOne
             services.AddControllers();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
--- Authentication/ApiTwo/Startup.cs
+++ Authentication/ApiTwo/Startup.cs
@@ -12,13 +12,21 @@ namespace ApiTwo
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAuthentication("Bearer")
+                .AddJwtBearer("Bearer", config =>
+                {
+                    config.Authority = "http://localhost:57313/";
+
+                    config.Audience = "ApiTwo";
+                });
+
+            services.AddHttpClient();
+
+            services.AddControllers();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -28,12 +36,13 @@ namespace ApiTwo
 
             app.UseRouting();
 
+            app.UseAuthentication();
+
+            app.UseAuthorization();
+
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapDefaultControllerRoute();
             });
         }
     }
