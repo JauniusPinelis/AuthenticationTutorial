484c9e29ef7a15eb45eef15d0542c191e89780dd
Jaunius Pinelis <jaunius@aml-analytics.com>
2020-09-01 11:38:50 +0300

    javascript redirects to login

--- Authentication/src/JavascriptClient/wwwroot/sign-in.js
+++ Authentication/src/JavascriptClient/wwwroot/sign-in.js
@@ -1,1 +1,24 @@
-﻿
+﻿var createState = function () {
+    return "SessionValueMakeItAbitLongerasdsfdfdfdvfdfdflgfjklmlmlm";
+}
+
+var createNonce = function () {
+    return "NonceValuefdffffffffffffffffffffffffffffgtggggtytytyytyt"
+}
+
+
+var signIn = function () {
+
+    var redirectUri = "https://localhost:44374/SignIn";
+    var responseType = "id_token%20token";
+    var scope = "openid ApiOne";
+
+    var authUrl = `/connect/authorize/callback?client_id=client_id_js?redirect_uri=${encodeURIComponent(redirectUri)}?response_type=${responseType}?scope=${encodeURIComponent(scope)}?nonce=${createNonce()}?state=${createState()}`;
+    var returnUrl = encodeURIComponent(authUrl);
+
+    console.log(authUrl);
+    console.log(returnUrl);
+
+    window.location.href =
+        "https://localhost:44397/Auth/Login?ReturnUrl=" + returnUrl;
+}
