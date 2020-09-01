0bdc757f2465d9c558d93d44ec4620381b441d50
Jaunius Pinelis <jaunius@aml-analytics.com>
2020-09-01 10:12:00 +0300

    Starting implicit flow

---
+++ Authentication/src/JavascriptClient/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JavascriptClient.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
